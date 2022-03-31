1045db4db81d06bccec54ebe573bc592825a7246
Adrian Spinosi <74249696+AdrianSP1@users.noreply.github.com>
2022-03-31 17:54:35 -0300

    interface usuarioRepository interface import CrudRepository-Service arrayList obtenerusuario que me trae todos los usuarios ingresados

--- apirest/src/main/java/com/example/apirest/repositories/usuarioRepository.java
+++ apirest/src/main/java/com/example/apirest/repositories/usuarioRepository.java
@@ -1,5 +1,13 @@
 package com.example.apirest.repositories;
 
-public class usuarioRepository {
+import org.springframework.stereotype.Repository;
+
+import com.example.apirest.models.usuarioModel;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+@Repository
+public interface usuarioRepository extends CrudRepository<usuarioModel, Long>{
 
 }
--- apirest/src/main/java/com/example/apirest/services/usuarioService.java
+++ apirest/src/main/java/com/example/apirest/services/usuarioService.java
@@ -1,5 +1,20 @@
 package com.example.apirest.services;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.apirest.models.usuarioModel;
+import com.example.apirest.repositories.usuarioRepository;
+
+@Service
 public class usuarioService {
+	@Autowired
+	usuarioRepository UsuarioRepository;
+	
+	public ArrayList<usuarioModel> obtenerUsuario(){
+		return (ArrayList<usuarioModel>) UsuarioRepository.findAll();
+	}
 
 }
